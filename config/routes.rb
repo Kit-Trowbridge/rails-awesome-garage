08b7bcb7bfed2292f5b0dce73ba0eaa82b575491
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-16 12:29:07 +0000

    amending reviews routes and adding to car show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cars#index'
-  resources :cars, only: :show
+  resources :cars, only: :show do
+    resources :reviews, only: [:new, :create]
+  end
 end
