c9c39bd024130e192dee01131457b8ae602f8503
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-16 15:29:58 +0000

    modified reviews controller create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cars#index'
   resources :cars, only: :show do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: :create
   end
 end
