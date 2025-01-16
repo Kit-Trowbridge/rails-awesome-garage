8bba252c0c2c933b9dd46abb78754fdf68e9e700
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-16 15:33:00 +0000

    first go at favorites routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'cars#index'
   resources :cars, only: :show do
     resources :reviews, only: :create
+    resources :favorites, only: :create
   end
+  resources :favorites, only: [:index, :destroy]
 end
