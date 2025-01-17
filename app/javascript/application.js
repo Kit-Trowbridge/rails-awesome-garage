1f1692e4440bffc4b8ac441921b30802c7a5eefd
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-17 14:54:12 +0000

    tried messing around with JS - leaving for now. Also added review styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,15 @@ import "controllers"
 
 import "bootstrap"
 import "@popperjs/core"
+
+
+review_section = Document.querySelector(".review-section")
+review_form = Document.querySelector(".review-form")
+comment_button = Document.querySelector(".comment-btn")
+// how to do click or hover
+// review_section.addEventListener("click", (event) => {
+//   event.currentTarget.classList.toggle("show");
+//   })
+review_section.addEventListener("click", (event) => {
+  console.log("clicked")
+  })
